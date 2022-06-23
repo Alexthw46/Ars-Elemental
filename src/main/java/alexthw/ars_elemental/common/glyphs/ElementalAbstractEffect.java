58948a6353d8b2fd0c3d183f38065b9921c48f9a
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2022-06-23 09:47:17 +0200

    Quick fix
    Signed-off-by: Alexthw46 <77152778+Alexthw46@users.noreply.github.com>

--- src/main/java/alexthw/ars_elemental/common/glyphs/ElementalAbstractEffect.java
+++ src/main/java/alexthw/ars_elemental/common/glyphs/ElementalAbstractEffect.java
@@ -2,11 +2,12 @@ package alexthw.ars_elemental.common.glyphs;
 
 import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
 
+import static alexthw.ars_elemental.ArsElemental.prefix;
+
 public abstract class ElementalAbstractEffect extends AbstractEffect {
 
     public ElementalAbstractEffect(String tag, String description) {
-        super("glyph_" + tag, description);
-        //super(prefix(tag), description);
+        super(prefix("glyph_" + tag), description);
     }
 
 }
--- src/main/java/alexthw/ars_elemental/common/glyphs/ElementalAbstractForm.java
+++ src/main/java/alexthw/ars_elemental/common/glyphs/ElementalAbstractForm.java
@@ -2,8 +2,11 @@ package alexthw.ars_elemental.common.glyphs;
 
 import com.hollingsworth.arsnouveau.api.spell.AbstractCastMethod;
 
+import static alexthw.ars_elemental.ArsElemental.prefix;
+
 public abstract class ElementalAbstractForm extends AbstractCastMethod {
     public ElementalAbstractForm(String tag, String description) {
-        super("glyph_" + tag, description);
+        super(prefix("glyph_" + tag), description);
     }
+
 }
