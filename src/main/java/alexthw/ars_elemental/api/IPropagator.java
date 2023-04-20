320ec6d3ceced2796f698d737b3e77f8aff40ff9
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-04-20 14:45:37 +0200

    Fix leaves fast texture
    Signed-off-by: Alexthw46 <77152778+Alexthw46@users.noreply.github.com>

--- src/main/java/alexthw/ars_elemental/api/IPropagator.java
+++ src/main/java/alexthw/ars_elemental/api/IPropagator.java
@@ -12,18 +12,21 @@ import org.jetbrains.annotations.Nullable;
 public interface IPropagator {
 
     default void copyResolver(HitResult rayTraceResult, Level world, @Nullable LivingEntity shooter, SpellStats stats, SpellContext spellContext, SpellResolver resolver) {
-        // Cancel the current spell and get the remaining glyphs
-        if (spellContext.getCurrentIndex() == 1) return;
         spellContext.setCanceled(true);
+        //keep the propagator glyph to ensure augments are applied
         spellContext.setCurrentIndex(spellContext.getCurrentIndex() - 1);
         Spell newSpell = spellContext.getRemainingSpell();
         if (newSpell.isEmpty()) return;
-        // Create a new context and resolver with the remaining glyphs
         SpellContext newContext = spellContext.clone().withSpell(newSpell);
-        SpellResolver newResolver = resolver.getNewResolver(newContext);
-        // Propagate the new spell
+        SpellResolver newResolver = new SpellResolver(newContext) {
+            //remove the leftover propagator before resolving
+            @Override
+            protected void resolveAllEffects(Level world) {
+                this.spellContext.getSpell().recipe.remove(0);
+                super.resolveAllEffects(world);
+            }
+        };
         propagate(world, rayTraceResult, shooter, stats, newResolver);
-
     }
 
     void propagate(Level world, HitResult hitResult, LivingEntity shooter, SpellStats stats, SpellResolver resolver);
