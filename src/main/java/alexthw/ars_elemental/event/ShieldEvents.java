69b56c2dd852b00b891b6d3a5722f8d39e438135
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-09-11 12:10:27 +0200

    Reflect Warden Attacks

--- src/main/java/alexthw/ars_elemental/event/ShieldEvents.java
+++ src/main/java/alexthw/ars_elemental/event/ShieldEvents.java
@@ -7,14 +7,19 @@ import alexthw.ars_elemental.registry.ModRegistry;
 import com.hollingsworth.arsnouveau.common.entity.EntityProjectileSpell;
 import com.hollingsworth.arsnouveau.common.items.EnchantersShield;
 import com.hollingsworth.arsnouveau.common.spell.casters.ReactiveCaster;
-import com.hollingsworth.arsnouveau.setup.registry.CapabilityRegistry;
-import com.hollingsworth.arsnouveau.setup.registry.EnchantmentRegistry;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.ShieldBlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -63,6 +68,50 @@ public class ShieldEvents {
             }
         }
 
+    }
+
+    @SubscribeEvent
+    public static void onSonicImpact(LivingAttackEvent event){
+        if (!(event.getEntity() instanceof Player player && player.getCommandSenderWorld() instanceof ServerLevel level))
+            return;
+        ItemStack s = player.getOffhandItem();
+        //if the shield is an enchanters shield then check if the shield has the reactive enchantment and if the roll is successful then cast the spell
+        if (s.getItem() instanceof EnchantersShield && player.isBlocking() && s.getEnchantmentLevel(ModRegistry.MIRROR.get()) * .25 >= Math.random()) {
+            if (event.getSource().getSourcePosition() != null && event.getSource().getMsgId().equals(DamageSource.sonicBoom(event.getEntity()).getMsgId())) {
+
+                Vec3 vec3 = player.getViewVector(1.0F);
+                Vec3 vec31 = event.getSource().getSourcePosition().vectorTo(player.position()).normalize();
+                vec31 = new Vec3(vec31.x, 0.0D, vec31.z);
+                if (vec31.dot(vec3) < 0.0D)
+                    reflectSonicBoom(event, player, s, level);
+            }
+        }
+    }
+
+    private static void reflectSonicBoom(LivingAttackEvent event, Player player, ItemStack stack, ServerLevel level) {
+        event.setCanceled(true);
+
+        if (!(event.getSource().getEntity() instanceof LivingEntity reflectTo)) {
+            return;
+        }
+        Vec3 vec3 = player.getEyePosition();
+        Vec3 vec31 = reflectTo.position().add(0.0D, 1.6F, 0.0D).subtract(vec3);
+        Vec3 vec32 = vec31.normalize();
+
+        for(int i = 1; i < Mth.floor(vec31.length()) + 7; ++i) {
+            Vec3 vec33 = vec3.add(vec32.scale(i));
+            level.sendParticles(ParticleTypes.SONIC_BOOM, vec33.x, vec33.y, vec33.z, 1, 0.0D, 0.0D, 0.0D, 0.0D);
+        }
+
+        player.playSound(SoundEvents.WARDEN_SONIC_BOOM, 3.0F, 1.0F);
+        reflectTo.hurt(DamageSource.sonicBoom(player), 10.0F);
+        double d1 = 0.5D * (1.0D - reflectTo.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
+        double d0 = 2.5D * (1.0D - reflectTo.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
+        reflectTo.push(vec32.x() * d0, vec32.y() * d1, vec32.z() * d0);
+
+        CapabilityRegistry.getMana(player).ifPresent(mana -> mana.removeMana(150));
+        player.getCooldowns().addCooldown(stack.getItem(), 20 * stack.getEnchantmentLevel(ModRegistry.MIRROR.get()));
+        player.stopUsingItem();
     }
 
 }
