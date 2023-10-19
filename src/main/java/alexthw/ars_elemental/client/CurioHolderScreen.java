d729890da09b689a61e4945af1b557007e9eccd0
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-10-19 23:18:48 +0200

    Fix tooltips in bundlebags

--- src/main/java/alexthw/ars_elemental/client/CurioHolderScreen.java
+++ src/main/java/alexthw/ars_elemental/client/CurioHolderScreen.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import static alexthw.ars_elemental.ArsElemental.prefix;
@@ -33,13 +34,15 @@ public class CurioHolderScreen extends AbstractContainerScreen<CurioHolderContai
     @Override
     public void render(@NotNull GuiGraphics gui, int mouseX, int mouseY, float partialTicks) {
         super.render(gui, mouseX, mouseY, partialTicks);
+        renderTooltip(gui, mouseX, mouseY);
     }
 
 
     @Override
     protected void renderTooltip(@NotNull GuiGraphics gui, int mouseX, int mouseY) {
         if (this.menu.getCarried().isEmpty() && this.hoveredSlot != null && this.hoveredSlot.hasItem()) {
-            gui.renderTooltip(this.font, this.hoveredSlot.getItem(), mouseX, mouseY);
+            ItemStack itemstack = this.hoveredSlot.getItem();
+            gui.renderTooltip(this.font, this.getTooltipFromContainerItem(itemstack), itemstack.getTooltipImage(), itemstack, mouseX, mouseY);
         }
     }
 
