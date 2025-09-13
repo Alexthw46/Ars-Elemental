d77957fec5bbc5546619ebb208320522fb326e31
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-09-13 17:40:47 +0200

    support for multiple repulsion rituals in the potion effect

--- src/main/java/alexthw/ars_elemental/common/mob_effects/RepelEffect.java
+++ src/main/java/alexthw/ars_elemental/common/mob_effects/RepelEffect.java
@@ -3,6 +3,7 @@ package alexthw.ars_elemental.common.mob_effects;
 import alexthw.ars_elemental.registry.ModPotions;
 import com.alexthw.sauce.util.PosCarryMEI;
 import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,15 +14,21 @@ public class RepelEffect extends MobEffect {
         super(MobEffectCategory.NEUTRAL, new ParticleColor(255, 255, 0).getColor());
     }
 
+    private static void repel(LivingEntity entity, BlockPos origin) {
+        Vec3 dist = new Vec3(entity.getX() - origin.getX(), entity.getY() - origin.getY(), entity.getZ() - origin.getZ());
+        if (dist.length() < 15) {
+            entity.setDeltaMovement(entity.getDeltaMovement().add(dist.normalize()).scale(0.5F));
+            entity.hurtMarked = true;
+        }
+    }
+
     @Override
     public boolean applyEffectTick(LivingEntity entity, int pAmplifier) {
         if (!(entity.getEffect(ModPotions.REPEL) instanceof PosCarryMEI mei)) {
             return false;
         }
-        Vec3 dist = new Vec3(entity.getX() - mei.getOrigin().getX(), entity.getY() - mei.getOrigin().getY(), entity.getZ() - mei.getOrigin().getZ());
-        if (dist.length() < 15) {
-            entity.setDeltaMovement(entity.getDeltaMovement().add(dist.normalize()).scale(0.5F));
-            entity.hurtMarked = true;
+        for (var blockPos : mei.getAllOrigins()) {
+            repel(entity, blockPos);
         }
         return true;
     }
