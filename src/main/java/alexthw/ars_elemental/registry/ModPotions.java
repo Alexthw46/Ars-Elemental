c66a4f68fcb072e7544db297843c78eb82400318
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-02-15 16:28:24 +0100

    Fix magefire armor bypass not working as intended, changed oxidize to affect armor points directly

--- src/main/java/alexthw/ars_elemental/registry/ModPotions.java
+++ src/main/java/alexthw/ars_elemental/registry/ModPotions.java
@@ -75,7 +75,7 @@ public class ModPotions {
         VENOM = EFFECTS.register("venom", VenomEffect::new);
         ICE_SLIDE = EFFECTS.register("slip_feet", SlipFeetEffect::new);
         MIST = EFFECTS.register("mist", () -> new MistEffect().addAttributeModifier(Attributes.FOLLOW_RANGE, prefix("mist"), -0.75f, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
-        RUST = EFFECTS.register("rust", () -> new RustEffect().addAttributeModifier(Attributes.ARMOR_TOUGHNESS, prefix("rust"), -0.15f, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+        RUST = EFFECTS.register("rust", () -> new RustEffect().addAttributeModifier(Attributes.ARMOR, prefix("rust"), -0.15f, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
 
         ENDERFERENCE_POTION = POTIONS.register(potion("enderference"), () -> new Potion(new MobEffectInstance(ENDERFERENCE, 400)));
         LONG_ENDERFERENCE_POTION = POTIONS.register(longPotion("enderference"), () -> new Potion(new MobEffectInstance(ENDERFERENCE, 1200)));
