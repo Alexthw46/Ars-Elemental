426946f19edad991a55d55abfc58ac90e4a1d134
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-03-26 14:11:10 +0100

    Enable Spell Crit

--- src/main/java/alexthw/ars_elemental/ArsElemental.java
+++ src/main/java/alexthw/ars_elemental/ArsElemental.java
@@ -33,6 +33,7 @@ import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 import java.util.UUID;
 
 import static com.alexthw.sauce.Sauce.ENABLE_ENTHRALL;
+import static com.alexthw.sauce.Sauce.ENABLE_SPELL_CRIT;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ArsElemental.MODID)
@@ -46,6 +47,7 @@ public class ArsElemental {
     public ArsElemental(IEventBus modEventBus, ModContainer modContainer) {
         terrablenderLoaded = ModList.get().isLoaded("terrablender");
         ENABLE_ENTHRALL = true;
+        ENABLE_SPELL_CRIT = true;
         modContainer.registerConfig(ModConfig.Type.STARTUP, ConfigHandler.STARTUP_SPEC);
         modContainer.registerConfig(ModConfig.Type.COMMON, ConfigHandler.COMMON_SPEC);
         modContainer.registerConfig(ModConfig.Type.CLIENT, ConfigHandler.CLIENT_SPEC);
