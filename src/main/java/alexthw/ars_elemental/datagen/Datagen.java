757b94488305b48ab81f69597d39220c0e6ccb48
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-06-06 00:33:42 +0200

    Removal of patchouli leftovers after deprecation of ars wiki in favour of ars.guide

--- src/main/java/alexthw/ars_elemental/datagen/Datagen.java
+++ src/main/java/alexthw/ars_elemental/datagen/Datagen.java
@@ -49,7 +49,6 @@ public class Datagen {
         gen.addProvider(event.includeServer(), new AEGlyphProvider(gen));
         gen.addProvider(event.includeServer(), new AEApparatusProvider(gen));
         gen.addProvider(event.includeServer(), new AECrushProvider(gen));
-        gen.addProvider(event.includeServer(), new AEPatchouliProvider(gen));
 
         gen.addProvider(event.includeServer(), new AECurioProvider(output, existingFileHelper, provider));
 
